7565ffac372ced6abb56b906e7580b2b2b550ce7
Michael McCann <micanio@googlemail.com>
2022-05-30 17:03:47 +0100

    final commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,15 @@ import React from "react";
 import logo from './../../img/Flotilla_full_colour.png'
 import "./../../styles/header.scss"
 
+const leeds2019 = 281;
+const london2019 = 908;
+const edin2019 = 514;
+const total2019 = 1703;
+const leeds2020 = 81;
+const london2020 = 411;
+const edin2020 = 90;
+const total2020 = 582;
+
 export default class Header extends React.Component {
     render() {
         return (
@@ -16,7 +25,8 @@ export default class Header extends React.Component {
                             <h2>Leeds</h2>
                         </div>
                         <div className="cfScore">
-                            <p>81</p>
+                            <p className={"twenty" + (leeds2020 < leeds2019 ? " green" : " red" )}>{leeds2020}</p>
+                            <p className="nineteen">2019: <span>{leeds2019}</span></p>                        
                         </div>
                     </div>
                     <div className="score">
@@ -24,7 +34,8 @@ export default class Header extends React.Component {
                             <h2>London</h2>  
                         </div>
                         <div className="cfScore">
-                            <p>411</p>
+                            <p className={"twenty" + (london2020 < london2019 ? " green" : " red" )}>{london2020}</p>
+                            <p className="nineteen">2019: <span>{london2019}</span></p>
                         </div>
                     </div>
                     <div className="score">
@@ -32,7 +43,8 @@ export default class Header extends React.Component {
                             <h2>Edinburgh</h2>
                         </div>
                         <div className="cfScore">
-                            <p>90</p>
+                            <p className={"twenty" + (edin2020 < edin2019 ? " green" : " red" )}>{edin2020}</p>
+                            <p className="nineteen">2019: <span>{edin2019}</span></p>                        
                         </div>
                     </div>
                     <div className="score">
@@ -40,7 +52,8 @@ export default class Header extends React.Component {
                             <h2>Total Score</h2>
                         </div>
                         <div className="cfScore">
-                            <p>582</p>
+                            <p className={"twenty" + (total2020 < total2019 ? " green" : " red" )}>{total2020}</p>
+                            <p className="nineteen">2019: <span>{total2019}</span></p>
                         </div>
                     </div>
                 </div>
